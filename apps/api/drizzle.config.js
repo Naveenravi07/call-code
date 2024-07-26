f1786199ecaca5592632a58607a4da96dec09137
Naveenravi07 <gamerkidnav@gmail.com>
2024-07-26 23:57:25 +0530

    Setting up eslint

--- apps/api/drizzle.config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var drizzle_kit_1 = require('drizzle-kit');
-exports.default = (0, drizzle_kit_1.defineConfig)({
-  schema: './src/database/schema',
-  out: './migrations',
-  dialect: 'postgresql',
-});
