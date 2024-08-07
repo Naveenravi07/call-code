7bfb5fd6d6421a3bc874bc172fd33936e00cb026
Naveenravi07 <gamerkidnav@gmail.com>
2024-08-07 23:43:31 +0530

    Added tests

--- apps/api/src/middlewares/zod.middleware.ts
+++ apps/api/src/middlewares/zod.middleware.ts
@@ -1,20 +1,19 @@
 import { Request, Response } from 'express';
 import { z } from 'zod';
 
-export function zodMiddleWare(err: unknown, _req: Request, res: Response): void {
+export function zodMiddleWare(err: unknown, req: Request, res: Response): void {
   if (err instanceof z.ZodError) {
     res.status(400).json({
       error: err.flatten(),
     });
-    return;
   } else if (err instanceof Error) {
     const error = err as Error & { statusCode?: number };
     res.status(error.statusCode ?? 400).json({
       message: err.message,
     });
-    return;
+  } else {
+    res.status(500).json({
+      message: 'Internal server error',
+    });
   }
-  res.status(500).json({
-    message: 'Internal server error',
-  });
 }
