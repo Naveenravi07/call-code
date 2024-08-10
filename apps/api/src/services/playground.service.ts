ce280a8aa00adc82fc7555a3e21b0013f690cefc
Naveenravi07 <gamerkidnav@gmail.com>
2024-08-10 22:50:26 +0530

    playground starting

---
+++ apps/api/src/services/playground.service.ts
@@ -0,0 +1,15 @@
+import AWS from "aws-sdk"
+
+const ecsClient = new AWS.ECS({
+    accessKeyId: process.env.AWS_ACCESSKEY,
+    secretAccessKey: process.env.AWS_SECRETKEY
+})
+
+
+async function createPlayground(clusterName: string, taskId: string) {
+    let response = await ecsClient.runTask({
+        cluster: clusterName,
+        taskDefinition: taskId,
+        count: 1
+    }).promise()
+}
