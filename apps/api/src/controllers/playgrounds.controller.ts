6e075b530c09ef0ee23a8242d229625e8760bf41
Naveenravi07 <gamerkidnav@gmail.com>
2024-08-17 22:13:41 +0530

    Mount targets

--- apps/api/src/controllers/playgrounds.controller.ts
+++ apps/api/src/controllers/playgrounds.controller.ts
@@ -1,7 +1,9 @@
-import { createEFSForPlayground } from '$/services/playground.service';
+import { createEFSForPlayground,createMountTargetForPlayground } from '$/services/playground.service';
 import { Request, Response } from 'express';
 
 export async function createPlaygroundController(req: Request, res: Response) {
   const efs = await createEFSForPlayground();
+  if(efs.FileSystemId == undefined) return
+  const moountStat = await createMountTargetForPlayground(efs.FileSystemId)
   res.send(200);
 }
--- apps/api/src/services/playground.service.ts
+++ apps/api/src/services/playground.service.ts
@@ -1,28 +1,59 @@
-import AWS from 'aws-sdk';
-import { EFSClient, CreateFileSystemCommand } from '@aws-sdk/client-efs';
+import { EFSClient, CreateFileSystemCommand,CreateMountTargetCommand } from '@aws-sdk/client-efs';
+import { ECSClient, RunTaskCommand,MountPoint } from "@aws-sdk/client-ecs"
 
-const ecsClient = new AWS.ECS({
-  accessKeyId: process.env.AWS_ACCESSKEY,
-  secretAccessKey: process.env.AWS_SECRETKEY,
+const ecsClient = new ECSClient({
+    region:"us-west-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESSKEY ?? "",
+        secretAccessKey: process.env.AWS_SECRETKEY ?? ""
+    }
 });
 
 const efsClient = new EFSClient({
-  region: 'us-west-1',
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESSKEY ?? '',
-    secretAccessKey: process.env.AWS_SECRETKEY ?? '',
-  },
+    region: 'us-west-1',
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESSKEY ?? '',
+        secretAccessKey: process.env.AWS_SECRETKEY ?? '',
+    },
 });
 
 export async function createEFSForPlayground() {
-  const command = new CreateFileSystemCommand({
-    PerformanceMode: 'generalPurpose',
-    ThroughputMode: 'bursting',
-    Encrypted: true,
-    Tags: [{ Key: 'Purpose', Value: 'CodePlayground' }],
-  });
+    const command = new CreateFileSystemCommand({
+        PerformanceMode: 'generalPurpose',
+        ThroughputMode: 'bursting',
+        Encrypted: true,
+        Tags: [{ Key: 'Purpose', Value: 'CodePlayground' }],
+    });
 
-  const efsData = await efsClient.send(command);
-  console.log('EFS Created:', efsData.FileSystemId);
-  return efsData;
+    const efsData = await efsClient.send(command);
+    console.log('EFS Created:', efsData.FileSystemId);
+    return efsData;
+}
+
+export async function createMountTargetForPlayground(fileSystemId:string){
+    await new Promise(resolve => setTimeout(resolve,1000));
+    const createMountTargetParams = {
+        FileSystemId: fileSystemId,
+        SubnetId: "subnet-0a74b75b4a1c7412b",
+        SecurityGroups: ["sg-0e2a3312321fdcdfd"], 
+      };
+      const createMountTargetCommand = new CreateMountTargetCommand(createMountTargetParams);
+      const mountTargetData = await efsClient.send(createMountTargetCommand);
+      console.log(`Created Mount Target: ${mountTargetData.MountTargetId}`);
+}
+
+export async function runECSPlaygroundTask() {
+    let command = new RunTaskCommand({
+        cluster : 'codecall-cluster',
+        taskDefinition:'code-vite',
+        overrides:{
+            containerOverrides:[
+                {
+                    name:"vite",
+                    
+                    
+                }
+            ]
+        }
+    })
 }
