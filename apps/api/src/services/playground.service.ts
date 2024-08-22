b373cecb58237ce2b1b703db8b5d2031c19a9b98
Naveenravi07 <gamerkidnav@gmail.com>
2024-08-22 23:43:40 +0530

    Removed efs architecture

--- apps/api/src/services/playground.service.ts
+++ apps/api/src/services/playground.service.ts
@@ -1,5 +1,4 @@
-import { EFSClient, CreateFileSystemCommand, CreateMountTargetCommand } from '@aws-sdk/client-efs';
-import { ECSClient, RunTaskCommand, MountPoint } from '@aws-sdk/client-ecs';
+import { ECSClient, RunTaskCommand } from '@aws-sdk/client-ecs';
 
 const ecsClient = new ECSClient({
   region: 'us-west-1',
@@ -9,40 +8,7 @@ const ecsClient = new ECSClient({
   },
 });
 
-const efsClient = new EFSClient({
-  region: 'us-west-1',
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESSKEY ?? '',
-    secretAccessKey: process.env.AWS_SECRETKEY ?? '',
-  },
-});
-
-export async function createEFSForPlayground() {
-  const command = new CreateFileSystemCommand({
-    PerformanceMode: 'generalPurpose',
-    ThroughputMode: 'bursting',
-    Encrypted: true,
-    Tags: [{ Key: 'Purpose', Value: 'CodePlayground' }],
-  });
-
-  const efsData = await efsClient.send(command);
-  console.log('EFS Created:', efsData.FileSystemId);
-  return efsData;
-}
-
-export async function createMountTargetForPlayground(fileSystemId: string) {
-  await new Promise(resolve => setTimeout(resolve, 10000));
-  const createMountTargetParams = {
-    FileSystemId: fileSystemId,
-    SubnetId: 'subnet-0a74b75b4a1c7412b',
-    SecurityGroups: ['sg-0e2a3312321fdcdfd'],
-  };
-  const createMountTargetCommand = new CreateMountTargetCommand(createMountTargetParams);
-  const mountTargetData = await efsClient.send(createMountTargetCommand);
-  console.log(`Created Mount Target: ${mountTargetData.MountTargetId}`);
-}
-
-export async function runECSPlaygroundTask(fileSystemId: string) {
+export async function runECSPlaygroundTask() {
   let command = new RunTaskCommand({
     cluster: 'codecall-cluster',
     taskDefinition: 'code-vite',
@@ -51,30 +17,7 @@ export async function runECSPlaygroundTask(fileSystemId: string) {
       awsvpcConfiguration: {
         subnets: ['subnet-0a74b75b4a1c7412b'],
         securityGroups: ['sg-0e2a3312321fdcdfd'],
-      },
-    },
-    overrides: {
-      containerOverrides: [
-        {
-          name: 'vite',
-          environment: [
-            {
-              name: 'EFS_ID',
-              value: fileSystemId,
-            },
-          ],
-        },
-        {
-          name: 'ws',
-          environment: [
-            {
-              name: 'EFS_ID',
-              value: fileSystemId,
-            },
-          ],
-        },
-      ],
-    },
+      }},
   });
   const taskData = await ecsClient.send(command);
   if (taskData.tasks == undefined) return;
