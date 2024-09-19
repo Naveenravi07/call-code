cd0240d65a00dfa0dd273e28f263d211129f6f4c
Naveenravi07 <gamerkidnav@gmail.com>
2024-09-19 22:59:55 +0530

    Removed efs code

--- apps/api/src/controllers/playgrounds.controller.ts
+++ apps/api/src/controllers/playgrounds.controller.ts
@@ -1,14 +1,9 @@
 import {
-  createEFSForPlayground,
-  createMountTargetForPlayground,
   runECSPlaygroundTask,
 } from '$/services/playground.service';
 import { Request, Response } from 'express';
 
 export async function createPlaygroundController(req: Request, res: Response) {
-  const efs = await createEFSForPlayground();
-  if (efs.FileSystemId == undefined) return;
-  const moountStat = await createMountTargetForPlayground(efs.FileSystemId);
-  const taskStat = await runECSPlaygroundTask(efs.FileSystemId);
+  const taskStat = await runECSPlaygroundTask("code-vite3");
   res.send(200);
 }
--- apps/api/src/services/playground.service.ts
+++ apps/api/src/services/playground.service.ts
@@ -17,64 +17,19 @@ const efsClient = new EFSClient({
   },
 });
 
-export async function createEFSForPlayground() {
-  const command = new CreateFileSystemCommand({
-    PerformanceMode: 'generalPurpose',
-    ThroughputMode: 'bursting',
-    Encrypted: true,
-    Tags: [{ Key: 'Purpose', Value: 'CodePlayground' }],
-  });
 
-  const efsData = await efsClient.send(command);
-  console.log('EFS Created:', efsData.FileSystemId);
-  return efsData;
-}
 
-export async function createMountTargetForPlayground(fileSystemId: string) {
-  await new Promise(resolve => setTimeout(resolve, 10000));
-  const createMountTargetParams = {
-    FileSystemId: fileSystemId,
-    SubnetId: 'subnet-0a74b75b4a1c7412b',
-    SecurityGroups: ['sg-0e2a3312321fdcdfd'],
-  };
-  const createMountTargetCommand = new CreateMountTargetCommand(createMountTargetParams);
-  const mountTargetData = await efsClient.send(createMountTargetCommand);
-  console.log(`Created Mount Target: ${mountTargetData.MountTargetId}`);
-}
-
-export async function runECSPlaygroundTask(fileSystemId: string) {
+export async function runECSPlaygroundTask(taskName: string) {
   let command = new RunTaskCommand({
     cluster: 'codecall-cluster',
-    taskDefinition: 'code-vite',
+    taskDefinition: taskName,
     launchType: 'EC2',
     networkConfiguration: {
       awsvpcConfiguration: {
         subnets: ['subnet-0a74b75b4a1c7412b'],
         securityGroups: ['sg-0e2a3312321fdcdfd'],
       },
-    },
-    overrides: {
-      containerOverrides: [
-        {
-          name: 'vite',
-          environment: [
-            {
-              name: 'EFS_ID',
-              value: fileSystemId,
-            },
-          ],
-        },
-        {
-          name: 'ws',
-          environment: [
-            {
-              name: 'EFS_ID',
-              value: fileSystemId,
-            },
-          ],
-        },
-      ],
-    },
+    }
   });
   const taskData = await ecsClient.send(command);
   if (taskData.tasks == undefined) return;
