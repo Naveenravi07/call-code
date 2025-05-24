777070ac2df2e454729f798abc5f6f737dbd1271
Naveenravi07 <gamerkidnav@gmail.com>
2025-05-25 01:46:27 +0530

    fix: Removed redundant code

--- src/playgrounds/playgrounds.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { PlaygroundsService } from './playgrounds.service';
-
-@Controller('playgrounds')
-export class PlaygroundsController {
-  constructor(private readonly playgroundsService: PlaygroundsService) {}
-}
--- src/playgrounds/playgrounds.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PlaygroundsService } from './playgrounds.service';
-import { PlaygroundsController } from './playgrounds.controller';
-
-@Module({
-  controllers: [PlaygroundsController],
-  providers: [PlaygroundsService],
-})
-export class PlaygroundsModule {}
